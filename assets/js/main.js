18ea10caee199c7cde53a4032dc4a4b43afa72ed
Shubham Chaurasia <87766168+ShubhamChaurasia22@users.noreply.github.com>
2024-01-25 10:24:45 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+    // Navbar Menu dropdown
+
+    // Initial setup - hide the nav-list
+    $("#nav-close").addClass("display-none");
+    $(".nav-list").addClass("display-none");
+
+    // Toggle the nav-list and show the close button when the navbar-toggle is clicked
+    $("#nav-toggle").click(function() {
+        $(".nav-list").toggleClass("display-none");
+
+        // Use slideDown() to smoothly show the element
+        $(".nav-list").slideDown(400, function() {
+            $("#nav-toggle").toggleClass("display-none"); // Toggle the toggle button
+            $("#nav-close").toggleClass("display-none"); // Toggle the close button
+        });
+    });
+
+    // Hide the nav-list and close button when the navbar-close is clicked
+    $("#nav-close").click(function() {
+        // Use slideUp() to smoothly hide the element
+        $(".nav-list").slideUp(400, function() {
+            $("#nav-toggle").toggleClass("display-none"); // Toggle the toggle button
+            $("#nav-close").toggleClass("display-none"); // Toggle the close button
+        });
+    });
+});
+
+
+
+
+
+// Slider
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-carousel', {
+        perPage: 2,
+        breakpoints: {
+            380: {
+                perPage: 2,
+            },
+            786: {
+                perPage: 3,
+            },
+            980: {
+                perPage: 4,
+            },
+            1440: {
+                perPage: 8,
+            },
+          },
+    } ).mount();
+} );
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#footer-carousel', {
+        perPage: 2,
+        breakpoints: {
+            380: {
+                perPage: 2,
+            },
+            786: {
+                perPage: 3,
+            },
+            980: {
+                perPage: 4,
+            },
+            1440: {
+                perPage: 8,
+            },
+          },
+    } ).mount();
+} );
